edd6703da9805b2446a658ce8f61cff99a6b857c
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-02-19 17:22:15 -0600

    finished abstract methods

--- src/edu/mu/Vehicle/Vehicle.java
+++ src/edu/mu/Vehicle/Vehicle.java
@@ -14,6 +14,10 @@ public abstract class Vehicle {
 	protected double gasTankCapacity;
 	protected StartMechanism startType;
 	
+	public abstract void calculateMaintenaceCost(double distance);
+	public abstract void calculateFuelEfficency(double distance, double fuelPrice);
+	public abstract void startEngine();
+	
 	public Vehicle(String brand, String make, long modelYear, double price, VehicleColor color, 
 			FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity,
             StartMechanism startType) 
