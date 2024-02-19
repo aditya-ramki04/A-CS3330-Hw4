ca18bd6d4cf54ce16f5c670de9e4bb460c3de8c5
Matthew Robison <mattrobi04@gmail.com>
2024-02-19 17:15:49 -0600

    finished creating the variables since ENUM was implemented and setter

--- src/edu/mu/Vehicle/Vehicle.java
+++ src/edu/mu/Vehicle/Vehicle.java
@@ -6,5 +6,29 @@ public abstract class Vehicle {
 	protected String make;
 	protected long modelYear;
 	protected double price;
+	protected VehicleColor color;
+	protected FuelType fuelType;
+	protected double mileage;
+	protected double mass;
+	protected int cylinders;
+	protected double gasTankCapacity;
+	protected StartMechanism startType;
+	
+	public Vehicle(String brand, String make, long modelYear, double price, VehicleColor color, 
+			FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity,
+            StartMechanism startType) 
+	{
+	 this.brand = brand;
+	 this.make = make;
+	 this.modelYear = modelYear;
+	 this.price = price;
+	 this.color = color;
+	 this.fuelType = fuelType;
+	 this.mileage = mileage;
+	 this.mass = mass;
+	 this.cylinders = cylinders;
+	 this.gasTankCapacity = gasTankCapacity;
+	 this.startType = startType;
+}
 	
 }
