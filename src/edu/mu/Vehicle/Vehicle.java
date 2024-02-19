bf4c17bb7d48b772a74f6507f6a5d2efe15060d4
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-02-19 17:55:51 -0600

    created getters and setters

--- src/edu/mu/Vehicle/Vehicle.java
+++ src/edu/mu/Vehicle/Vehicle.java
@@ -33,6 +33,74 @@ public abstract class Vehicle {
 	 this.cylinders = cylinders;
 	 this.gasTankCapacity = gasTankCapacity;
 	 this.startType = startType;
-}
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public long getModelYear() {
+		return modelYear;
+	}
+	public void setModelYear(long modelYear) {
+		this.modelYear = modelYear;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public VehicleColor getColor() {
+		return color;
+	}
+	public void setColor(VehicleColor color) {
+		this.color = color;
+	}
+	public FuelType getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(FuelType fuelType) {
+		this.fuelType = fuelType;
+	}
+	public double getMileage() {
+		return mileage;
+	}
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+	public double getMass() {
+		return mass;
+	}
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+	public int getCylinders() {
+		return cylinders;
+	}
+	public void setCylinders(int cylinders) {
+		this.cylinders = cylinders;
+	}
+	public double getGasTankCapacity() {
+		return gasTankCapacity;
+	}
+	public void setGasTankCapacity(double gasTankCapacity) {
+		this.gasTankCapacity = gasTankCapacity;
+	}
+	public StartMechanism getStartType() {
+		return startType;
+	}
+	public void setStartType(StartMechanism startType) {
+		this.startType = startType;
+	}
+	
+	
 	
 }
